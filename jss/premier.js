68d648fbe3bdcb0458d798e6261bc3f3a34899df
Vicenté Llavata <81528481+Toumic@users.noreply.github.com>
2022-04-07 11:10:29 +0200

    Merge remote-tracking branch 'origin/master'

--- jss/premier.js
+++ jss/premier.js
@@ -1,7 +1,7 @@
 /*Javascript pour calculer rapidement l'origine d'un nombre*/
 
 function origine(num){
-    let win = window.open();
+    // let win = window.open();
     num = parseInt(num)
     let retour = ""
     let tip = num % 6;
@@ -14,20 +14,20 @@ function origine(num){
             let p1er = prime[i];
             if (num % p1er === 0){
                 neo = false;
-                win.document.write(' > Num commun base: ', p1er.toString(), "<br>");
+                // win.document.write(' > Num commun base: ', p1er.toString(), "<br>");
                 retour += "Base 0: " + p1er;
             }
         }
     }
     if (car * car === num) {
         neo = false;
-        win.document.write('Num carré: ', car.toString(), "<br>");
+        // win.document.write('Num carré: ', car.toString(), "<br>");
         if (retour){ retour += "<br>"}
         retour += "Carré" + car;
     }
     if (di7 * 7 === num) {
         neo = false;
-        win.document.write('Num commun 7: ', di7.toString(), "<br>");
+        // win.document.write('Num commun 7: ', di7.toString(), "<br>");
         if (retour){ retour += "<br>"}
         retour += "Base 7: " + di7;
     }
@@ -38,14 +38,14 @@ function origine(num){
             let qui = Math.trunc(num / fw);
             if (qui * fw === num){
                 qi = num / qui;
-                win.document.write(' QUI:', qui.toString());
+                // win.document.write(' QUI:', qui.toString());
                 retour += "Multiple" + qui;
                 break;
             }
         }
         if (qi === 0)
         {
-            win.document.write('Num premier: ', num.toString());
+            // win.document.write('Num premier: ', num.toString());
             retour = "Premier" + num;
         }
     }
